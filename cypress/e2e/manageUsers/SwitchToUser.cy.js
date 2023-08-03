3705b897f5776b59bb34550951849770f6c6a7af
trodriguescorrea <trodriguescorrea@repay.com>
2023-08-03 14:19:33 +0100

    updating project

---
+++ cypress/e2e/manageUsers/SwitchToUser.cy.js
@@ -0,0 +1,50 @@
+/// <reference types="Cypress" />
+
+import actionsPage from '../../support/actions';
+import loginPage from '../../support/pages/login'
+import homePage from '../../support/pages/home' 
+import authenticatePage from '../../support/pages/authentication'
+
+import findEditPage from '../../support/pages/manageUsers/findEdit' 
+
+
+describe("Manage Users Test Page", () => {
+
+    beforeEach(function () {
+        cy.fixture("gatewayCredentials").as('user')
+
+        cy.get("@user").then((user) => {
+            cy.setCookie(user.cookiesName,user.cookiesValue)
+
+        })
+        loginPage.goToLoginPage()
+
+        
+        loginPage.checkLoginPage()
+
+        cy.get("@user").then((user) => {
+            loginPage.login(user.login, user.password)
+            // authenticatePage.authenticate(user.code)
+        })
+
+        homePage.checkHomePage()
+        const optionMenu = 'Manage Users'
+        const subMenuOption = 'Find/Edit'
+       
+    
+        homePage.goToOption(optionMenu, subMenuOption)
+        findEditPage.checkFindEditPage()
+    })
+
+    it("Check Manage Users > findEditPage - Search for User", () => {
+
+        // findEditPage.clickRefreshButton()
+        findEditPage.searchByName("1233177079")
+
+
+    });
+
+    
+
+})
+
--- cypress/support/pages/manageUsers/findEdit/index.js
+++ cypress/support/pages/manageUsers/findEdit/index.js
@@ -4,12 +4,14 @@ let refreshButton = "button[id*='Refresh']"
 let gridTable = '#grid_find_user2' 
 let userNameInput = "input[id*='Username']"
 
-let firstNameInput = "input[id$='FilterTextBox_column']"
-let lastNameInput = "input[id$='FilterTextBox_column1']"
+let lastNameInput  = "input[id$='FilterTextBox_column']"
+let firstNameInput = "input[id$='FilterTextBox_column1']"
 let resellerDropdown = "input[id*='Reseller_Input']"
 let statusDropdown = "input[id*='StatusFilter_Input']" 
 let calendarButton = ".rcCalPopup" 
 let filterButton = ".rgFilter" 
+let firstItem = "tbody tr.rgRow a#grid_find_user2_ctl00_ctl04_UserLink" 
+
 
 
 
@@ -39,7 +41,16 @@ class findEdit {
         
     }
 
-    
+    searchByName(text){
+        actionsPage.inputIframeText(userNameInput, text)
+        actionsPage.submitIframe(userNameInput)
+        cy.wait(10000)
+        actionsPage.clickIframe(firstItem)
+    }
+
+    clickFirstItem(){
+        
+    }
 
     clickRefreshButton(){
         actionsPage.clickIframe(refreshButton)
