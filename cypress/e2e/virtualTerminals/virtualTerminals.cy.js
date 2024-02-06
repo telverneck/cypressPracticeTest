1f4c2fa8f2a9bd01af0e7725dce202d617b86959
trodriguescorrea <trodriguescorrea@repay.com>
2024-02-06 14:05:54 +0000

    Add Virtual Terminal
    + Virtual Terminal script

---
+++ cypress/e2e/virtualTerminals/virtualTerminals.cy.js
@@ -0,0 +1,55 @@
+/// <reference types="Cypress" />
+
+import actionsPage from '../../support/actions';
+import loginPage from '../../support/pages/login'
+import homePage from '../../support/pages/home' 
+import authenticatePage from '../../support/pages/authentication'
+
+import findEditResellers from '../../support/pages/manageResellers/findEdit' 
+
+
+describe("Virtual Terminals Test Page", () => {
+
+    beforeEach(function () {
+        cy.fixture("gatewayCredentials").as('user')
+
+        cy.get("@user").then((user) => {
+            cy.setCookie(user.cookiesName,user.cookiesValue)
+
+        })
+
+        loginPage.goToLoginPage()
+
+        
+        loginPage.checkLoginPage()
+
+        cy.get("@user").then((user) => {
+            loginPage.login(user.login, user.password)
+            // authenticatePage.authenticate(user.code)
+        })
+
+        homePage.checkHomePage()
+        const optionMenu = 'Manage Resellers'
+        const subMenuOption = 'Find/Edit' 
+        
+        
+        homePage.goToOption(optionMenu, subMenuOption)
+        findEditResellers.checkFindEditResellersPage()
+    })
+    
+    it("Check Virtual Terminals > Card", () => {
+
+        findEditResellers.filterBy("EqualTo")
+        findEditResellers.searchByName("test")
+        findEditResellers.checkResultsTable()
+
+
+
+    });
+
+    
+
+    
+
+})
+
