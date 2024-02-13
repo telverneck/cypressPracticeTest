7e5446817c2ed2dc861bddc3524cf5eced2e5ea7
trodriguescorrea <trodriguescorrea@repay.com>
2024-02-13 14:01:50 +0000

    Fix Preferences Folder
    Fix Preferences Folder

---
+++ cypress/e2e/preferences/security.cy.js
@@ -0,0 +1,48 @@
+/// <reference types="Cypress" />
+
+import actionsPage from '../../support/actions';
+import loginPage from '../../support/pages/login'
+import homePage from '../../support/pages/home' 
+import authenticatePage from '../../support/pages/authentication'
+
+import twoFactorPage from '../../support/pages/preferences/twoFactor' 
+
+
+describe("Preferences - Security", () => {
+
+    beforeEach(function () {
+        cy.fixture("gatewayCredentials").as('user')
+
+        cy.get("@user").then((user) => {
+            cy.setCookie(user.cookiesName,user.cookiesValue)
+
+        })
+        loginPage.goToLoginPage()
+
+        
+        loginPage.checkLoginPage()
+
+        cy.get("@user").then((user) => {
+            loginPage.login(user.login, user.password)
+            // authenticatePage.authenticate(user.code)
+        })
+
+        homePage.checkHomePage()
+        const optionMenu = 'Preferences'
+        const subMenuOption = 'Two Factor'
+       
+    
+        homePage.goToOption(optionMenu, subMenuOption)
+    })
+
+    it("Check Preferences > Security info", () => {
+
+        twoFactorPage.checkTwoFactorPage()
+
+
+    });
+
+    
+
+})
+
